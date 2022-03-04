4aff5ec12b52f16162cc3992ba8e0c41cb9979f0
Sooyoung Yoon <ayoooo240@gmail.com>
2022-03-04 16:21:25 +0900

    ✨ Coins 페이지 코인 리스트 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,84 @@
-import styled from "styled-components"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Title = styled.h1`
-  color: ${props => props.theme.accentColor}
-`;
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "usdt-tether",
+    name: "Tether",
+    symbol: "USDT",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
 
 export default function Coins() {
   return (
-    <Title>Coins</Title>
-  )
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 15px;
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  a {
+    display: block;
+    transition: color 0.3s ease-in-out;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor}
+    }
+  }
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;

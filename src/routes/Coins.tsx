1d78b502fac0e089641fa7eafc34e1fb17a6d4f0
Sooyoung Yoon <ayoooo240@gmail.com>
2022-03-04 20:16:22 +0900

    ✨ 코인 API에서 데이터 fetch

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,54 +1,52 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "usdt-tether",
-    name: "Tether",
-    symbol: "USDT",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 export default function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  console.log(coins);
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
     </Container>
   );
 }
 
 const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
   padding: 0 20px;
 `;
 
@@ -56,24 +54,24 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 10vh;
+  height: 15vh;
 `;
 
 const CoinList = styled.ul``;
 
 const Coin = styled.li`
   margin-bottom: 10px;
-  padding: 20px;
   border-radius: 15px;
   background-color: white;
   color: ${(props) => props.theme.bgColor};
   a {
     display: block;
+    padding: 20px;
     transition: color 0.3s ease-in-out;
   }
   &:hover {
     a {
-      color: ${(props) => props.theme.accentColor}
+      color: ${(props) => props.theme.accentColor};
     }
   }
 `;
@@ -81,4 +79,9 @@ const Coin = styled.li`
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
   font-size: 48px;
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
 `;
